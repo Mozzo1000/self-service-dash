eb0833b766e80b7035eda569a68ab63372ecd993
Andreas Backström <andreas@simplymozzo.se>
2022-05-13 16:32:27 +0200

    Add tooltip to example

--- examples/version.py
+++ examples/version.py
@@ -12,9 +12,13 @@ class GetVersion:
     def setVersion(self):
         if platform.system() == "Windows":
             self.parent.change_title("Version: Windows " + platform.release())
+            self.parent.set_tooltip("Build: " + platform.version())
         elif platform.system() == "Darwin":
             self.parent.change_title("Version: macOS " + platform.release())
+            self.parent.set_tooltip("Build: " + platform.version())
         elif platform.system() == "Linux":
             self.parent.change_title("Version: Linux " + platform.release())
+            self.parent.set_tooltip("Build: " + platform.version())
         else:
             self.parent.change_title("Version: Can't find version number")
+            self.parent.set_tooltip("Build: Can't find build number")
