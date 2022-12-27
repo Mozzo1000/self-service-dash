8aab8345d05c7446d3729fbffff8f0692287c9bf
Andreas Backström <andreas@simplymozzo.se>
2022-12-27 18:41:27 +0100

    Add open on left click

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from PyQt5.QtWidgets import QApplication, QMenu, QSystemTrayIcon
-from PyQt5.QtGui import QIcon, QDesktopServices
+from PyQt5.QtGui import QIcon, QDesktopServices, QCursor
 from PyQt5.QtCore import QUrl
 import sys
 import json
 from config import Config
 from menuitem import MenuItem
+
 
 class App:
     def __init__(self):
@@ -13,28 +14,38 @@ class App:
 
         if self.config.get_mode() == "tray":
             icon = QIcon(self.config.get_item("icon"))
-            menu = QMenu()
-            menu.setToolTipsVisible(True)
+            self.menu = QMenu()
+            self.menu.setToolTipsVisible(True)
 
             for item in self.config.get_item("menu"):
-                menu_it = MenuItem(parent=menu,
-                                    title=self.config.get_item("title", parent=item),
-                                    icon=self.config.get_item("icon", parent=item),
-                                    type=self.config.get_item("type", parent=item),
-                                    action=self.config.get_item("action", parent=item),
-                                    options=self.config.get_item("options", parent=item))
-                menu.addAction(menu_it.get_action())
+                menu_it = MenuItem(parent=self.menu,
+                                   title=self.config.get_item(
+                                       "title", parent=item),
+                                   icon=self.config.get_item(
+                                       "icon", parent=item),
+                                   type=self.config.get_item(
+                                       "type", parent=item),
+                                   action=self.config.get_item(
+                                       "action", parent=item),
+                                   options=self.config.get_item("options", parent=item))
+                self.menu.addAction(menu_it.get_action())
 
             self.tray = QSystemTrayIcon()
+            # self.tray.activated.connect(self.showMenuOnTrigger)
             self.tray.setToolTip(self.config.get_item("title"))
             self.tray.setIcon(icon)
-            self.tray.setContextMenu(menu)
+            self.tray.setContextMenu(self.menu)
             self.tray.show()
+
+    def showMenuOnTrigger(self, reason):
+        if reason == QSystemTrayIcon.Trigger:
+            self.menu.popup(QCursor.pos())
 
     def run(self):
         # Enter Qt application main loop
         self.app.exec_()
         sys.exit()
+
 
 if __name__ == "__main__":
     app = App()
