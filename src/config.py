4a8d184967b1bb5bd1e0377d8bd79e12e692f707
Andreas Backström <andreas@simplymozzo.se>
2022-12-27 19:26:57 +0100

    Fix config loading

--- src/config.py
+++ src/config.py
@@ -2,16 +2,17 @@ import json
 import sys
 import os
 
+
 class Config:
-    def __init__(self, config_file="../examples/settings.json"):
+    def __init__(self, config_file="examples/settings.json"):
         os.chdir(os.path.dirname(os.path.realpath(config_file)))
         try:
-            with open(config_file, "r") as f:
+            with open(os.path.basename(config_file), "r") as f:
                 self.raw_config = json.load(f)
         except OSError:
             print(f"Failed to open {config_file}")
             sys.exit()
-    
+
     def get_mode(self):
         if "mode" in self.raw_config:
             if self.raw_config["mode"] == "tray":
@@ -31,5 +32,5 @@ class Config:
         if item in parent:
             return parent[item]
         else:
-            print(f"Key {item} does not exist in json file.")
+            #print(f"Key {item} does not exist in json file.")
             return None
